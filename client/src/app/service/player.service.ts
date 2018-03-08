2bf5d749d877fbce5de3285b36d8ac49a7223f1b
Green-Star <alexandre.sahuc@cpy.re>
2018-03-08 23:41:14 +0100

    Move class declarations to models folder

--- client/src/app/service/player.service.ts
+++ client/src/app/service/player.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core'
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 
-import { Player } from '../player'
+import { Player } from '../models'
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
