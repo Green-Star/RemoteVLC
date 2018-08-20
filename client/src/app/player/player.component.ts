35a5fd4a2d95b22a288576f4a57501408b782cc1
Alexandre Sahuc <alexandre.sahuc@cpy.re>
2018-08-20 13:47:24 +0200

    Remove subscription in PlayerComponent since we don't need it

--- client/src/app/player/player.component.ts
+++ client/src/app/player/player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, AfterViewChecked, ViewChild } from '@angular/core'
+import { Component, OnInit, ViewChild } from '@angular/core'
 import { PlayerData, Track } from '../../../../shared'
 import { PlayerService } from '../service'
 import { TimerComponent } from '../timer/timer.component'
@@ -10,14 +10,9 @@ import { Subscription, Observable } from 'rxjs'
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.css']
 })
-export class PlayerComponent implements OnInit, AfterViewInit, AfterViewChecked {
+export class PlayerComponent implements OnInit {
   player: PlayerData
-  selectedVideoTrack: Track
-  selectedAudioTrack: Track
-  selectedSubtitleTrack: Track
   @ViewChild(TimerComponent) timerComponent: TimerComponent
-
-  trackComponentSubscription: Subscription
 
   @ViewChild('video') videoTrackComponent: TrackComponent
   @ViewChild('audio') audioTrackComponent: TrackComponent
@@ -28,74 +23,10 @@ export class PlayerComponent implements OnInit, AfterViewInit, AfterViewChecked 
   ngOnInit() {
     this.player = undefined
     this.playerService.init().subscribe(data => this.updatePlayerData(data))
-    this.trackComponentSubscription = undefined
-  }
-
-  ngAfterViewInit() {
-    console.log('AfterViewInit')
-
-    if (!this.videoTrackComponent) return
-  }
-
-  ngAfterViewChecked() {
-    /* Est-ce qu'on ne pourrait pas juste ignorer la modif des tracks au final ? */
-    console.log('AfterViewChecked')
-
-    if (!this.videoTrackComponent) return
-
-      /* A corriger, ne fonctionne qu'une fois */
-    if (!this.trackComponentSubscription) {
-      /*
-      let playerData$: Observable<PlayerData> = this.videoTrackComponent.getData()
-
-      if (playerData$) {
-        this.trackComponentSubscription = playerData$.subscribe(data => {
-        console.log('subscribe in PlayerComponent')
-        console.warn('this.trackcomponentSubscription')
-        if (this.trackComponentSubscription) console.warn('DEfined')
-          else console.warn('undefined')
-        this.updatePlayerData(data)
-        this.trackComponentSubscription.unsubscribe()
-        this.trackComponentSubscription = undefined
-      })
-        console.warn('END OF SUBSCRIBE')
-      }
-      else 
-      {
-        console.log('playerDate undefined')
-
-      }
-      */
-
-/*
-      this.trackComponentSubscription = this.videoTrackComponent.getData()
-      .subscribe(data => {
-        console.log('subscribe in PlayerComponent')
-        this.updatePlayerData(data)
-
-        /* Ca n'a pas trop l'air de marcher (-> update playerData ?) ... */
-/*
-        this.trackComponentSubscription.unsubscribe()
-        this.trackComponentSubscription = undefined
-      })
-*/
-    }
   }
 
   updatePlayerData(data: PlayerData) {
       this.player = data
-      /*
-      this.selectedVideoTrack = this.getSelectedVideoTrack()
-      this.selectedAudioTrack = this.getSelectedAudioTrack()
-      this.selectedSubtitleTrack = this.getSelectedSubtitleTrack()
-      */
-
-console.log('UPDATE DATA')
-//console.warn(JSON.stringify(this.playerService))
-/*
-      if (this.timerComponent === undefined) return
-      this.timerComponent.updateTimer(this.player.time)
-    */
 
       if (this.timerComponent) this.timerComponent.updateTimer(this.player.time)
       if (this.videoTrackComponent) this.videoTrackComponent.setTracks(this.player.tracks.video)
@@ -103,48 +34,30 @@ console.log('UPDATE DATA')
       if (this.subtitleTrackComponent) this.subtitleTrackComponent.setTracks(this.player.tracks.subtitle)
   }
 
-  getSelectedVideoTrack() {
-    return this.player.tracks['video'].find(track => track.selected === true)
-  }
-  setSelectedVideoTrack(newTrack: Track) {
-    this.selectedVideoTrack = newTrack
-    this.setTrack('video', newTrack)
-  }
-  getSelectedAudioTrack() {
-    return this.player.tracks['audio'].find(track => track.selected === true)
-  }
-  setSelectedAudioTrack(newTrack: Track) {
-    this.selectedAudioTrack = newTrack
-    this.setTrack('audio', newTrack)
-  }
-  getSelectedSubtitleTrack() {
-    return this.player.tracks['subtitle'].find(track => track.selected === true)
-  }
-  setSelectedSubtitleTrack(newTrack: Track) {
-    this.selectedSubtitleTrack = newTrack
-    this.setTrack('subtitle', newTrack)
-  }
-
   isPlaying() {
     return this.player.isPlaying
   }
+
   pause() {
     this.playerService.pause().subscribe(data => {
       this.updatePlayerData(data)
       this.timerComponent.stopTimer()
     })
   }
+
   play() {
     this.playerService.play().subscribe(data => {
       this.updatePlayerData(data)
       this.timerComponent.startTimer()
     })
   }
+
   setTime(seconds: number) {
     if (seconds < 0) seconds = 0
     if (seconds > this.player.length) seconds = this.player.length
     this.playerService.setTime(seconds).subscribe(data => this.updatePlayerData(data))
   }
+
   addTime(seconds: number) {
     this.playerService.addTime(seconds).subscribe(data => this.updatePlayerData(data))
   }
@@ -152,14 +65,12 @@ console.log('UPDATE DATA')
   volumeDown() {
     this.playerService.volumeDown().subscribe(data => this.updatePlayerData(data))
   }
+
   volumeUp() {
     this.playerService.volumeUp().subscribe(data => this.updatePlayerData(data))
   }
+
   setVolume(volume: number) {
     this.playerService.setVolume(volume).subscribe(data => this.updatePlayerData(data))
-  }
-
-  setTrack(type: string, track: Track) {
-    this.playerService.setTrack(type, track.id).subscribe(data => this.updatePlayerData(data))
   }
 }
--- client/src/app/track/track.component.ts
+++ client/src/app/track/track.component.ts
@@ -12,24 +12,15 @@ export class TrackComponent implements OnInit {
   @Input() type: string
   @Input() tracks: Track[]
   selectedTrack: Track
-  data: Observable<PlayerData>
 
-  constructor(private playerService: PlayerService) { console.log('constructor')}
+  constructor(private playerService: PlayerService) {}
 
   ngOnInit() {
-    console.log('Child component type: '+this.type)
     this.selectedTrack = this.getSelectedTrack()
   }
 
-  getData(): Observable<PlayerData> {
-    return this.data
-  }
-
   updateValue(track: Track): void {
-    this.playerService.setTrack(this.type, track.id).subscribe(data => {
-      console.log('Subscribe in track')
-      this.data = of(data)
-    })
+    this.playerService.setTrack(this.type, track.id).subscribe()
   }
 
   setTracks(tracks: Track[]) {
