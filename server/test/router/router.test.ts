1bfdf95aa728a7270b17becb59cf7da2ce6cfa2f
Alexandre Sahuc <alexandre.sahuc@cpy.re>
2018-09-02 00:08:17 +0200

    Add mocha

---
+++ server/test/router/router.test.ts
@@ -0,0 +1,13 @@
+import 'mocha'
+import { expect } from 'chai'
+
+import { Router } from '../../router'
+
+describe('My first test', () => {
+
+  it('should be my first pending test')
+  it('should be my first passed test', (done) => {
+    done()
+  })
+
+})
