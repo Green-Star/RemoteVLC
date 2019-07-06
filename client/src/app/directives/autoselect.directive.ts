50b2dfda3a75fbfaf1dc476052b9cb3e1011620c
Alexandre Sahuc <alexandre.sahuc@cpy.re>
2019-07-06 22:42:58 +0200

    Add autoselect directive logic

--- client/src/app/directives/autoselect.directive.ts
+++ client/src/app/directives/autoselect.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appAutoselect]'
 })
 export class AutoselectDirective {
+  private selected: boolean = false
 
-  constructor() { }
+  constructor(
+    private el: ElementRef
+  ) {}
 
+  @HostListener('click') onclick() {
+    if (this.selected) return
+    this.selected = true
+    this.select()
+  }
+
+  @HostListener('blur') onBlur() {
+    this.selected = false
+  }
+
+  private select() {
+    this.el.nativeElement.select()
+  }
 }
