79951ffe4882b94dccd73a2a5e4d7a3585d3d167
Alexandre Sahuc <alexandre.sahuc@cpy.re>
2019-07-07 14:31:35 +0200

    Use onFocus listener

--- client/src/app/directives/autoselect.directive.ts
+++ client/src/app/directives/autoselect.directive.ts
@@ -4,23 +4,12 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
   selector: '[appAutoselect]'
 })
 export class AutoselectDirective {
-  private selected: boolean = false
 
   constructor(
     private el: ElementRef
   ) {}
 
-  @HostListener('click') onclick() {
-    if (this.selected) return
-    this.selected = true
-    this.select()
-  }
-
-  @HostListener('blur') onBlur() {
-    this.selected = false
-  }
-
-  private select() {
+  @HostListener('focus') onFocus() {
     this.el.nativeElement.select()
   }
 }
